d73222453c8c2f137cd2be6dc6bfce0675cffbef
Kyle Raney <raney24fcb@gmail.com>
2019-03-08 18:44:55 -0500

    svg script changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -32,17 +32,44 @@ $(document).ready(function () {
 function makeSVG(tag, attrs) {
   var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
   for (var k in attrs) {
+
     el.setAttribute(k, attrs[k]);
   }
   return el;
 };
 
+function parseSVG(s) {
+
+}
+
 $(document).ready(function() {
-  $(".dotted > span").each(function(index) {
+  $(".dotted").each(function(index) {
     console.log($(this).width());
-    $(this).attr("id", "svg_" + index);
-    var line = makeSVG('line', {x1: 0.5, y1: 0, x2: 0.5, y2: 100});
-    document.getElementById('svg_' + index).appendChild(line);
+    var child_span = $(this).children('span');
+    child_span.attr("id", "span_" + index);
+    var lgx_heading = $(this).parents('.lgx-heading-area');
+    lgx_heading.attr("id", "h_" + index);
+    
+    var viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var whitespace_width = viewport_width - child_span.width();
+    var offset_width = child_span.width() / 2;
+    var calc_width = whitespace_width / 2;
+    var line = makeSVG('line', {
+      x1: 0, 
+      y1: 0.5, 
+      x2: 100, 
+      y2: 0.5,
+      'stroke-dasharray': '1 5',
+      'stroke-width': '5%'
+    });
+    var svg_el = makeSVG('svg', {
+      viewBox: "0 0 100 1", 
+      width: lgx_heading.width(), 
+      height: $(this).height(), 
+      id: 'svg_' + index
+    });
+    svg_el.appendChild(line);
+    document.getElementById('h_' + index).insertAdjacentElement("afterbegin", svg_el);
     
   });
   
