451260cb13299c6a847987c619ac54df47f46ff1
Kyle Raney <raney24fcb@gmail.com>
2019-04-27 00:30:39 -0400

    add testimonials

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -117,8 +117,15 @@ $(document).ready(function() {
     document.getElementById( 'h_' + index ).insertAdjacentElement("afterbegin", svg_el);
     
   });
+});
 
-  
-  
-  
+
+$(document).ready(function() {
+  function fade($ele) {
+    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+        var $next = $(this).next('.quote');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+  });
+  }
+  fade($('.quoteLoop > .quote').first());
 });
