d6fd5e97b37debc0f05416b22164cc07c5a1ae51
ClassyDragon <cgandeg@gmail.com>
2020-08-05 13:31:23 -0400

    Graph

--- assets/js/bootcamp-isa-calculator.js
+++ assets/js/bootcamp-isa-calculator.js
@@ -46,50 +46,18 @@ function setupGraph(numbers) {
         document.getElementsByClassName("graph")[0].remove();
     }
 
-   var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    var margin = {top: 30, right: 30, bottom: 70, left: 60},
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
-    // append the svg object to the body of the page
-    /*
-    var svg = d3.select("#mygraph")
-    .append("svg")
+    let svg = d3.select("#mygraph")
+        .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
-        .attr("class", "graph")
-    .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-            */
+        .append("g")
+        .attr("transform", "translate(30, 30)")
+        //.call(slider)
 
-            /*
-    let data = [
-        {Country: "Gross Salary", Value: numbers.grossSalary},
-        {Country: "Gross Monthly Income", Value: numbers.grossMonthlyIncome},
-        {Country: "Minimum Monthly Payments", Value: numbers.minMonthlyPayments},
-        {Country: "ISA Percentage", Value: numbers.isaPercentage},
-        {Country: "Cap", Value: numbers.cap},
-        {Country: "Monthly Payment", Value: numbers.monthlyPayment}
-    ];
-    */
-    let data = [
-        {Payments: "# of Monthly Payments", Price: ""}
-    ]
-
-    // Parse the Data
-    // X axis
-    /*
-    var x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(data.map(function(d) { return d.Country; }))
-    .padding(0.2);
-    svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
-        */
     let slider = d3
         .sliderHorizontal()
         .min(0)
@@ -98,35 +66,46 @@ function setupGraph(numbers) {
         .width(300)
         .displayValue(false)
 
-    // Add Y axis
-    /*
-    var y = d3.scaleLinear()
-    .domain([0, 1000])
-    .range([ height, 0]);
+    let data = [
+        {Payments: 5, Price: 500}
+    ]
+
+    // X axis
+    var x = d3.scaleBand()
+        .range([ 0, width ])
+        .domain(data.map(function(d) { return d.Country; }))
+        .padding(0.2);
+
     svg.append("g")
-    .call(d3.axisLeft(y));
-    */
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([0, 1000])
+        .range([ height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    //svg.call(slider)
+    svg.append("g")
+        .call(slider)
+        .attr("transform", `translate(30, ${height})`);
 
     // Bars
-    let svg = d3.select("#mygraph")
-        .append("svg")
-        .attr("width", 500)
-        .attr("height", 500)
-        .append("g")
-        .attr("transform", "translate(30, 30)")
-        .call(slider)
-    /*
     svg.selectAll("mybar")
-    .data(data)
-    .enter()
-    .append("rect")
+        .data(data)
+        .enter()
+        .append("rect")
         //.attr("x", function(d) { return x(d.Payments); })
         //.attr("x", function(d) { return slider(); })
-        .call(slider)
+        .attr("x", function(d) { return x(d.Payments);})
         .attr("y", function(d) { return y(d.Price); })
         //.attr("width", x.bandwidth())
-        .attr("width", 1000)
-        .attr("height", function(d) { return height - y(d.Value); })
+        .attr("width", 30)
+        .attr("height", function(d) { return height - y(d.Price); })
         .attr("fill", "#69b3a2");
-        */
 }
