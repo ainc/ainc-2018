74dac4625aac9fc3b35fd8bdb070bb9b5e8a2622
chienleow <chienleow1@gmail.com>
2020-10-14 17:52:58 -0400

    Counter Up from Code Pen working

--- assets/js/impact-fullpage.js
+++ assets/js/impact-fullpage.js
@@ -3,3 +3,12 @@ new fullpage('#fullpage', {
     autoScrolling:true,
     navigation:true
 });
+
+// Jquery Counter-Up from Codepen
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 2000
+  });
+  $('.counter').addClass('animated fadeInDownBig');
+  $('h3').addClass('animated fadeIn');
