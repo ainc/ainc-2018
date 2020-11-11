ffb5c1af2a91ee3915630aa77261a68c5e4beb9a
chienleow <chienleow1@gmail.com>
2020-11-11 11:11:36 -0500

    Add images to second page of Apax

--- assets/js/impact-counterup.js
+++ assets/js/impact-counterup.js
@@ -1,22 +1,21 @@
 // Jquery Counter-Up from Codepen
-$('.counter').counterUp({
-  delay: 10,
-  time: 2000
-});
-$('.counter').addClass('animated fadeInDownBig');
-$('h3').addClass('animated fadeIn');
-
-// $("div").scroll(function() { 
-//   $('.counter').counterUp( {
-//     delay: 10,
-//     time: 2000
-//   });
+// $('.counter').counterUp({
+//   delay: 10,
+//   time: 2000
 // });
-
-// // $('.counter').counterUp({
-// //   delay: 10,
-// //   time: 2000
-// // });
 // $('.counter').addClass('animated fadeInDownBig');
 // $('h3').addClass('animated fadeIn');
+
+$(window).scroll(function() {
+  var hT = $('#div').offset().top,
+      hH = $('#div').outerHeight(),
+      wH = $(window).height(),
+      wS = $(this).scrollTop();
+  if (wS > (hT+hH-wH)){
+      console.log('H1 on the view!');
+  }
+});
+
+// 1. offset scroll
+// 2. turn off counter after running once?
 
