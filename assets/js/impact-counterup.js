09cae8e3f5697b3432e29478ddca6f3e8825e8eb
chienleow <chienleow1@gmail.com>
2020-11-11 10:12:12 -0500

    Add another file for counterup js

---
+++ assets/js/impact-counterup.js
@@ -0,0 +1,22 @@
+// Jquery Counter-Up from Codepen
+$('.counter').counterUp({
+  delay: 10,
+  time: 2000
+});
+$('.counter').addClass('animated fadeInDownBig');
+$('h3').addClass('animated fadeIn');
+
+// $("div").scroll(function() { 
+//   $('.counter').counterUp( {
+//     delay: 10,
+//     time: 2000
+//   });
+// });
+
+// // $('.counter').counterUp({
+// //   delay: 10,
+// //   time: 2000
+// // });
+// $('.counter').addClass('animated fadeInDownBig');
+// $('h3').addClass('animated fadeIn');
+
