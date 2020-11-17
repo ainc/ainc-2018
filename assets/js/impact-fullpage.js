01d4deedc8818bc7a73d9bc193d5435ffc518bdb
chienleow <chienleow1@gmail.com>
2020-11-17 15:20:10 -0500

    add href to title for anchors

--- assets/js/impact-fullpage.js
+++ assets/js/impact-fullpage.js
@@ -2,15 +2,15 @@ new fullpage('#fullpage', {
 	//options here
     autoScrolling: true,
     navigation: true,
-    anchors: ['section s1, section s2, section s3, section s4, section s5']
+    anchors: ['section s1', 'section s2', 'section s3', 'section s4', 'section s5'],
     controlArrows: false,
     slidesNavigation: true,
     slidesNavPosition: 'bottom',
     
-    afterLoad: function(origin, destination, direction){
-        if(origin.anchor == 'section s3'){
-            alert("HELLO!!! IT WORKS!")
-        }
-    }
+    // afterLoad: function(origin){
+    //     if(origin.anchor == 'section s3'){
+    //         alert("HELLO!!! IT WORKS!")
+    //     }
+    // }
 });
 
