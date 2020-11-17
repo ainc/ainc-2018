7671970f2cb698220093d7f947ed7fe78b3b6dc0
chienleow <chienleow1@gmail.com>
2020-11-17 15:10:45 -0500

    add anchors to fullpage js

--- assets/js/impact-counterup.js
+++ assets/js/impact-counterup.js
@@ -6,16 +6,16 @@
 // $('.counter').addClass('animated fadeInDownBig');
 // $('h3').addClass('animated fadeIn');
 
-$(window).scroll(function() {
-  console.log("HELLO!");
-  var hT = $('#something').offset().top,
-      hH = $('#something').outerHeight(),
-      wH = $(window).height(),
-      wS = $(this).scrollTop();
-  if (wS > (hT+hH-wH)){
-      console.log('H1 on the view!');
-  }
-});
+// $(window).scroll(function() {
+//   console.log(document.body.offsetHeight);
+//   var hT = $(window).offset().top,
+//       hH = $(window).outerHeight(),
+//       wH = $(window).height(),
+//       wS = $(this).scrollTop();
+//   if (wS > (hT+hH-wH)){
+//       console.log('H1 on the view!');
+//   }
+// });
 
 // 1. offset scroll
 // 2. turn off counter after running once?
--- assets/js/impact-fullpage.js
+++ assets/js/impact-fullpage.js
@@ -2,8 +2,15 @@ new fullpage('#fullpage', {
 	//options here
     autoScrolling: true,
     navigation: true,
+    anchors: ['section s1, section s2, section s3, section s4, section s5']
     controlArrows: false,
     slidesNavigation: true,
-	  slidesNavPosition: 'bottom'
+    slidesNavPosition: 'bottom',
+    
+    afterLoad: function(origin, destination, direction){
+        if(origin.anchor == 'section s3'){
+            alert("HELLO!!! IT WORKS!")
+        }
+    }
 });
 
