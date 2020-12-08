cf3ad80f1810e0f1ca0021f8df6c9fa3a2493b8c
chienleow <chienleow1@gmail.com>
2020-12-07 21:49:48 -0500

    add notes

--- assets/js/impact-fullpage.js
+++ assets/js/impact-fullpage.js
@@ -9,7 +9,10 @@ new fullpage('#fullpage', {
     
     afterLoad: function(o, d, dir){
         // const allSections = ('section1' || 'section2' || 'section3' || 'section4' || 'section5')
-        if(d.anchor == 'section3' && d.anchor == 'section4'){
+        // switch statement for JS
+        // put the counterup as another function
+        // make it work first, refactor later (checking if d.anchor is the section the user is looking at)
+        if(d.anchor == 'section3' || d.anchor == 'section4'){
             $('.counter').counterUp({
                 delay: 10,
                 time: 2000
