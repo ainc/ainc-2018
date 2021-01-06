9ee1787db6163e5e0138f93018ffbae6637beee2
chienleow <chienleow1@gmail.com>
2021-01-06 12:30:18 -0500

    coworking right side content

--- assets/js/impact-fullpage.js
+++ assets/js/impact-fullpage.js
@@ -7,24 +7,7 @@ new fullpage('#fullpage', {
     slidesNavigation: true,
     slidesNavPosition: 'bottom',
     
-    // afterLoad: function(o, d, dir){
-    //     // switch statement for JS
-    //     // put the counterup as another function
-    //     // make it work first, refactor later (checking if d.anchor is the section the user is looking at)
-    //     if(d.anchor == 'section3' || d.anchor == 'section4'){
-    //         $('.counter').counterUp({
-    //             delay: 10,
-    //             time: 2000
-    //         });
-    //         $('.counter').addClass('animated fadeInDownBig');
-    //         $('h3').addClass('animated fadeIn');
-    //     }
-    // }
-    
     afterLoad: function(origin, destination, direction){
-        // switch statement for JS
-        // put the counterup as another function
-        // make it work first, refactor later (checking if d.anchor is the section the user is looking at)
         switch(destination.anchor){
             case 'section3':
                 counterMethod("s3");
