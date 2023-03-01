6502a519ac232820e32eec6cfc7b0d257e53e1a1
Will Garman <wgarman702@gmail.com>
2023-03-01 16:03:08 -0500

    remoce console logs from popups

--- assets/js/modal-popup.js
+++ assets/js/modal-popup.js
@@ -1,12 +1,8 @@
 function displayPopup() {
   if (!window.sessionStorage.getItem("popupShown")) {
-    console.log("popup not shown");
+    // console.log("popup not shown");
     $( '#modal-popup' ).modal('show');
     window.sessionStorage.setItem("popupShown", true);
-  }
-  else {
-    console.log("popup shown");
-
   }
 }
 
